132e24138a989fbfeaffbc1a4e5a3d45b0ced8f5
willfroggg <58962597+a896536351@users.noreply.github.com>
2022-06-06 08:42:59 -0700

    Update gcc.sh

--- Final_project/gcc.sh
+++ Final_project/gcc.sh
@@ -6,13 +6,13 @@ apt install build-essential -y
 
 echo
 
-echo $gcc -S -masm=intel sample3.c -o test
+echo $ gcc -S -masm=intel sample3.c -o test
 
 gcc -S -masm=intel sample3.c -o test
 
 echo ---------------------------------------------------------------
 
-echo $cat test
+echo $ cat test
 
 echo
 
@@ -20,19 +20,19 @@ cat test
 
 echo ---------------------------------------------------------------
 
-echo $as -o test.o test
+echo $ as -o test.o test
 
 echo
 
 as -o test.o test
 
-echo $ld -o test -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/x86_64-linux-gnu/crtn.o test.o -lc  
+echo $ ld -o test -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/x86_64-linux-gnu/crtn.o test.o -lc  
 
 ld -o test -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/x86_64-linux-gnu/crtn.o test.o -lc  
 
 echo
 
-echo $ls-l
+echo $ ls-l
 
 ls -l
 
