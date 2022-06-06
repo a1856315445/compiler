98a93b7b3ff3935b6097577a89c417b5b507f70d
willfroggg <58962597+a896536351@users.noreply.github.com>
2022-06-06 08:44:01 -0700

    Update llvm.sh

--- Final_project/llvm.sh
+++ Final_project/llvm.sh
@@ -1,6 +1,6 @@
-echo $clang -S -emit-llvm sample3.c cat 
+echo $ clang -S -emit-llvm sample3.c cat 
 
-echo $sample3.ll
+echo $ sample3.ll
 
 echo
 
@@ -10,7 +10,7 @@ cat sample3.ll
 
 echo -------------------------------------------------------------------------
 
-echo $lli sample3.ll
+echo $ lli sample3.ll
 
 echo
 
@@ -20,9 +20,9 @@ echo
 
 echo -------------------------------------------------------------------------
 
-echo $opt -S -globalopt -loop-simplify -mem2reg sample3.ll
+echo $ opt -S -globalopt -loop-simplify -mem2reg sample3.ll
 
-echo $llc sample3.ll
+echo $ llc sample3.ll
 
 echo
 
@@ -32,7 +32,7 @@ llc sample3.ll
 
 echo -------------------------------------------------------------------------
 
-echo $cat sample3.s
+echo $ cat sample3.s
 
 echo
 
@@ -40,13 +40,13 @@ cat sample3.s
 
 echo -------------------------------------------------------------------------
 
-echo $as sample3.s -o sample3.o
+echo $ as sample3.s -o sample3.o
 
 as sample3.s -o sample3.o
 
 echo
 
-echo $ld -o sample3 -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o  /usr/lib/x86_64-linux-gnu/crtn.o sample3.o -lc  
+echo $ ld -o sample3 -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o  /usr/lib/x86_64-linux-gnu/crtn.o sample3.o -lc  
 
 ld -o sample3 -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o  /usr/lib/x86_64-linux-gnu/crtn.o sample3.o -lc  
 
