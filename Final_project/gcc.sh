3d75679d0716476b2e6ea21dfe689e7d762ec3f4
kevinboy666 <70878493+kevinboy666@users.noreply.github.com>
2022-06-27 22:03:10 +0800

    add annotation

--- Final_project/gcc.sh
+++ Final_project/gcc.sh
@@ -1,13 +1,11 @@
-
-
 apt update -y
-
+#install build-essential
 apt install build-essential -y
 
 echo
 
 echo $ gcc -S -masm=intel sample3.c -o test
-
+#get assembly for x86
 gcc -S -masm=intel sample3.c -o test
 
 echo ---------------------------------------------------------------
@@ -23,11 +21,11 @@ echo ---------------------------------------------------------------
 echo $ as -o test.o test
 
 echo
-
+#get object file from assembly
 as -o test.o test
 
 echo $ ld -o test -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/x86_64-linux-gnu/crtn.o test.o -lc  
-
+#get exe file from object file
 ld -o test -dynamic-linker /lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/x86_64-linux-gnu/crtn.o test.o -lc  
 
 echo
